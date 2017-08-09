e559924256ad2131f40bd106d63121597ab3a682
Gaurav Joshi <joshisgaurav@gmail.com>
2017-08-09 13:21:44 +0530

    Update metadata.rb

--- winSvr/metadata.rb
+++ winSvr/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures winSvr'
 long_description 'Installs/Configures winSvr'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-depends 'windows_feature', '~> 1.0.23'
+depends 'windows', '~> 3.1.1'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbooks page when
